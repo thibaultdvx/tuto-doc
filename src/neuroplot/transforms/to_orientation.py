a0b71a4bb3a0b607e534ce44b905359e43c5adbb
thibaultdvx <thibault.devarax@icm-institute.org>
2025-07-03 17:04:07 +0200

    fix format issues

--- src/neuroplot/transforms/to_orientation.py
+++ src/neuroplot/transforms/to_orientation.py
@@ -3,8 +3,14 @@ from torchio.transforms import ToOrientation as BaseToOrientation
 
 class ToOrientation(BaseToOrientation):
     def __init__(self, orientation: str = "RAS") -> None:
-        assert "R" in orientation or "L" in orientation, "'orientation' must contain 'R' (Right) or 'L' (Left)"
-        assert "A" in orientation or "P" in orientation, "'orientation' must contain 'A' (Anterior) or 'L' (Posterior)"
-        assert "S" in orientation or "I" in orientation, "'orientation' must contain 'S' (Superior) or 'I' (Inferior)"
+        assert (
+            "R" in orientation or "L" in orientation
+        ), "'orientation' must contain 'R' (Right) or 'L' (Left)"
+        assert (
+            "A" in orientation or "P" in orientation
+        ), "'orientation' must contain 'A' (Anterior) or 'L' (Posterior)"
+        assert (
+            "S" in orientation or "I" in orientation
+        ), "'orientation' must contain 'S' (Superior) or 'I' (Inferior)"
         assert len(orientation) == 3, "'orientation' must contain 3 letters"
         super().__init__(orientation=orientation)
